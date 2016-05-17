c27f268ca030d246058e073db51c35c66c1ca760
Chuan <c_p_827@Hotmail.com>
2016-05-17 03:03:41 -0400

    Switched to ui-router

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,91 @@
-var meanTodo = angular.module('meanTodo', ['homeController', 'aboutController', 'contactController', 'reverseFilter', 'passwordMatchDirective', 'passwordStrengthDirective', 'fileModelDirective','multipartFormService', 'LogoutController', 'LoginController', 'SignupController', 'NavbarController', 'todoController', 'todoService', 'toastr','ngRoute', 'ngMaterial', 'satellizer']);
+var meanTodo = angular.module('meanTodo', ['ngStorage', 'postServices', 'ui.router', 'postsController', 'homeController', 'aboutController', 'contactController', 'reverseFilter', 'passwordMatchDirective', 'passwordStrengthDirective', 'fileModelDirective','multipartFormService', 'LogoutController', 'LoginController', 'SignupController', 'NavbarController', 'todoController', 'todoService', 'toastr','ngRoute', 'ngMaterial', 'satellizer']);
 
 
-meanTodo.config(['$routeProvider', '$authProvider', function ($routeProvider, $authProvider) {
+meanTodo.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$provide', function ($urlRouterProvider, $stateProvider, $locationProvider, $provide) {
+    $locationProvider.html5Mode(true);
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl : 'partials/home.html',
+            controller : 'homeCtrl'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl : 'partials/about.html',
+            controller : 'aboutCtrl',
+            resolve: {
+                loginRequired: loginRequired
+            }
+        })
+        .state('contact', {
+            url: '/contact',
+            templateUrl : 'partials/contact.html',
+            controller : 'contactCtrl',
+            resolve: {
+                loginRequired: loginRequired
+            }
+        })
+        .state('todoList', {
+            url: '/todoList',
+            templateUrl: 'partials/todolist.html',
+            controller: 'todoCtrl',
+            resolve: {
+                loginRequired: loginRequired
+            }
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl',
+            resolve: {
+                skipIfLoggedIn: skipIfLoggedIn
+            }
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'partials/signup.html',
+            controller: 'SignupCtrl',
+            resolve: {
+                skipIfLoggedIn: skipIfLoggedIn
+            }
+        })
+        .state('logout', {
+            url: '/logout',
+            templateUrl: 'partials/home.html',
+            controller: 'LogoutCtrl'
+        })
+        .state('posts/:id', {
+            url: '/posts/:id',
+            templateUrl: 'partials/posts.html',
+            controller: 'PostsCtrl',
+            resolve: {
+                loginRequired: loginRequired,
+                posts: ['$localStorage', 'blogPosts', function($localStorage, blogPosts) {
+                    if (blogPosts.post.length != 0) {
+                        $localStorage.blogPosts = blogPosts.post;
+                    }
+                    return $localStorage.blogPosts;
+                }]
+            }
+        });
+
+    $provide.decorator('$state', function ($delegate, $stateParams) {
+        $delegate.forceReload = function () {
+            return $delegate.go($delegate.current, $stateParams, {
+                reload: true,
+                inherit: false,
+                notify: true
+            })
+        };
+        return $delegate;
+    }) ;
+
+
+
+    $urlRouterProvider.otherwise('/');
+}]);
+
+/*meanTodo.config(['$routeProvider', '$authProvider', function ($routeProvider, $authProvider) {
     $routeProvider
         .when('/', {
             templateUrl : 'partials/home.html',
@@ -45,8 +129,15 @@ meanTodo.config(['$routeProvider', '$authProvider', function ($routeProvider, $a
         .when('/logout', {
             templateUrl: 'partials/home.html',
             controller: 'LogoutCtrl'
+        })
+        .when('/posts/:id', {
+            templateUrl: 'partials/posts.html',
+            controller: 'PostsCtrl',
+            resolve: {
+                loginRequired: loginRequired
+            }
         });
-}]);
+}]);*/
 
 
 
